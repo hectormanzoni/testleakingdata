ad6d1dc95c65d90b227179133871550ee06fe25c
hectormanzoni <91678565+hectormanzoni@users.noreply.github.com>
2021-09-30 16:15:51 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,21 @@
+import requests
+
 print ( 'Im leaking Hypeflame data')
+
+user = 'eyJhbGci-OiJI-UzI1-NiIs-InR5cCI6IkpX'
+password = 'iaWF0Ijo-xNTE-2MjM-5MDI-yfQfmZS9ujYD'
+
+session = requests.Session()
+session.auth = (user, password)
+
+auth = session.get('https://api.agibank.com.br/')
+
+print(auth)
+print(auth.text)
+
+auth = session.get('https://api.hypeflame.com.br/')
+
+print(auth)
+print(auth.text)
+
+print ('Hello there')
