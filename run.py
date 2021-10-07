16048f8233505e12daf10fef7ca2199b31839612
hectormanzoni <91678565+hectormanzoni@users.noreply.github.com>
2021-10-07 18:16:26 -0300

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import requests
 print ( 'Im leaking Hypeflame data')
 
 user = 'eyJhbGci-OiJI-UzI1-NiIs-InR5cCI6IkpX'
-password = 'iaWF0Ijo-xNTE-2MjM-6MDI-yfQfmZS9ujYD'
+password = 'iaWF0Ijo-xNTE-3MjM-6MDI-yfQfmZS9ujYD'
 
 session = requests.Session()
 session.auth = (user, password)
